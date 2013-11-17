175ebb6593cf0e84569f183e8c2383a6c021e923
Evan Koschik <ekoschik@umich.edu>
2013-11-17 15:56:00 -0800

    improved sample

--- home.js
+++ home.js
@@ -14,28 +14,32 @@ var createContext = function() {
     return canvas.getContext("2d");
 }
 
-var draw_rect = function(x, y, width, height, fill) {
-	context.fillStyle=fill;
-	context.fillRect(x, y, width, height);
+var draw_rect = function(x, y, width, height, fill_color, border_color, border_width) {
+	context.beginPath();
+	context.rect(x, y, width, height);
+	context.fillStyle = fill_color;
+	context.fill();
+	context.lineWidth = border_width;
+	context.strokeStyle = border_color;
+	context.stroke();
 }
 
-var draw_circle = function(x, y, rad, fill, stroke) {
+var draw_circle = function(x, y, rad, fill_color, border_color, border_width) {
 	context.beginPath();
 	context.arc(x, y, rad, 0, 2 * Math.PI, false);
-	context.fillStyle = fill;
+	context.fillStyle = fill_color;
 	context.fill();
-	context.lineWidth = 1;
-	context.strokeStyle = stroke;
+	context.lineWidth = border_width;
+	context.strokeStyle = border_color;
 	context.stroke();
 }
 
 $(document).ready(function(){
-
 	context = createContext();
 
-	draw_rect(50, 25, 150, 100, 'green');
+	draw_rect(50, 25, 150, 100, 'green', 'black', 2);
 
-	draw_circle(190, 90, 45, 'red', 'black');
+	draw_circle(190, 90, 45, 'red', 'black', 3);
 
 });
 
